a6d5905ebc57281e71fa1bcf2a9b08bdb800d9b1
Axel Havukangas <axel@havukangas.fi>
2020-07-25 12:44:35 +0300

    Remove outdated cache files (#49)

--- src/__tests__/cache-test.js
+++ src/__tests__/cache-test.js
@@ -2,14 +2,22 @@
  * @flow
  */
 
+const fse = require('fs-extra');
 const path = require('path');
 
 const cache = require('../cache');
 const fsUtils = require('../utils/fs');
 
 describe('cache', () => {
-  const testfilePath = path.join(__dirname, 'testfile');
-  const nonexistingFilePath = path.join(__dirname, 'non-existent-file');
+  let tmpDir: string;
+  beforeEach(async () => {
+    tmpDir = await fse.mkdtemp('react-native-svg-asset-plugin');
+  });
+
+  afterEach(async () => {
+    await fse.remove(tmpDir);
+  });
+
   const config = {
     cacheDir: '.png-cache',
     scales: [1, 2, 3],
@@ -18,33 +26,96 @@ describe('cache', () => {
     lastModifiedTime: Date.now(),
   };
 
+  async function createFiles(files: { [name: string]: number }) {
+    return await Promise.all(
+      Object.keys(files).map(async (fileName) => {
+        const filePath = path.join(tmpDir, fileName);
+        await fse.writeFile(filePath, '');
+        const timestamp = Date.now() / 1000 - files[fileName];
+        await fse.utimes(filePath, timestamp, timestamp);
+        return filePath;
+      }),
+    );
+  }
+
   describe('isFileOutdated', () => {
     it('returns true for old files', async () => {
-      await fsUtils.updateLastModifiedTime(testfilePath);
+      const [filePath] = await createFiles({
+        'file.png': 10,
+      });
 
-      expect(
-        await cache.isFileOutdated(testfilePath, {
-          ...config,
-          lastModifiedTime: Date.now() + 10 * 1000,
-        }),
-      ).toBe(true);
+      expect(await cache.isFileOutdated(filePath, config)).toBe(true);
     });
 
     it('returns false for new files', async () => {
-      await fsUtils.updateLastModifiedTime(testfilePath);
+      const [filePath] = await createFiles({
+        'file.png': -10,
+      });
 
-      expect(
-        await cache.isFileOutdated(testfilePath, {
-          ...config,
-          lastModifiedTime: Date.now() - 10 * 1000,
-        }),
-      ).toBe(false);
+      expect(await cache.isFileOutdated(filePath, config)).toBe(false);
     });
 
     it('returns true for missing files', async () => {
-      expect(await cache.isFileOutdated(nonexistingFilePath, config)).toBe(
+      const nonexistentFilePath = path.join(tmpDir, 'nonexistent.png');
+      expect(await cache.isFileOutdated(nonexistentFilePath, config)).toBe(
         true,
       );
     });
   });
+
+  describe('scheduleCleanup', () => {
+    it('removes old images', async () => {
+      const [filePath] = await createFiles({
+        'new-image.png': 1 * 60 * 60,
+        'old-image.png': 2 * 24 * 60 * 60,
+      });
+
+      await callAndRunTimers(() => cache.isFileOutdated(filePath, config));
+
+      await waitFor(async () =>
+        expect(await fse.readdir(tmpDir)).toEqual(['new-image.png']),
+      );
+    });
+
+    it('does not remove old non-png', async () => {
+      const [filePath] = await createFiles({
+        'README.md': 30 * 24 * 60 * 60,
+      });
+
+      await callAndRunTimers(() => cache.isFileOutdated(filePath, config));
+
+      await waitFor(async () =>
+        expect(await fse.readdir(tmpDir)).toEqual(['README.md']),
+      );
+    });
+  });
 });
+
+/**
+ * Call function running all timers immediately.
+ * Useful for triggering background cleanups.
+ */
+async function callAndRunTimers(fn) {
+  jest.useFakeTimers();
+  await fn();
+  jest.runAllTimers();
+  jest.useRealTimers();
+}
+
+/**
+ * Call function repeatedly until it doesn't throw.
+ * Useful for waiting for test assertions to be fulfilled.
+ */
+async function waitFor(fn, timeout: number = 1000) {
+  const startTime = Date.now();
+  while (true) {
+    try {
+      await fn();
+      return;
+    } catch (error) {
+      if (Date.now() > startTime + timeout) {
+        throw error;
+      }
+    }
+  }
+}
--- src/cache.js
+++ src/cache.js
@@ -2,6 +2,9 @@
  * @flow strict-local
  */
 
+const path = require('path');
+const fse = require('fs-extra');
+
 const fsUtils = require('./utils/fs');
 
 import type { Config } from './config';
@@ -18,5 +21,79 @@ export async function isFileOutdated(
     outputFilePath,
   );
 
-  return outputLastWrittenTimeStamp < config.lastModifiedTime;
+  if (outputLastWrittenTimeStamp === 0) {
+    // File doesn't exist, or timestamps messed up
+    // Better to generate a new file
+    return true;
+  } else {
+    // File has been generated before, but has been removed from metro cache.
+
+    // We can safely remove all generated files in the directory which are
+    // older than the current file.
+    scheduleCleanup(outputFilePath, outputLastWrittenTimeStamp);
+
+    // Regenerate the file if it is older than the plugin configuration
+    return outputLastWrittenTimeStamp < config.lastModifiedTime;
+  }
+}
+
+// Only clean up each directory once per plugin instance.
+// Keep track of already cleaned directories here.
+const scheduledDirectoryCleanups = new Set();
+// Time to wait until cache cleanup is executed.
+// This gives the metro server time and resources to
+// process all assets before performing cleanup.
+const cleanupDelay = 5 * 60 * 1000;
+// Age that files must be older than last seen evicted file
+// for it to be removed.
+const fileAgeBuffer = 24 * 60 * 60 * 1000;
+
+/**
+ * Cleans up cache directory
+ */
+function scheduleCleanup(imageFilePath: string, timestamp: number): void {
+  const directoryPath = path.dirname(imageFilePath);
+  if (scheduledDirectoryCleanups.has(directoryPath)) {
+    // Directory has already been scheduled for processing by another call.
+    // Do nothing.
+    return;
+  }
+
+  scheduledDirectoryCleanups.add(directoryPath);
+  setTimeout(async () => {
+    try {
+      await removeFilesOlderThan(directoryPath, timestamp - fileAgeBuffer);
+    } catch {}
+  }, cleanupDelay);
+}
+
+/**
+ * Cleans up directory, removing all PNG images which are older than the given
+ * timestamp.
+ */
+async function removeFilesOlderThan(
+  directoryPath: string,
+  timestamp: number,
+): Promise<void> {
+  let fileNames;
+  try {
+    fileNames = await fse.readdir(directoryPath);
+  } catch {
+    return;
+  }
+
+  // Delibreately using slower sequential processing
+  // so that we don't hog resources from high prio work
+  for (const fileName of fileNames) {
+    const fileExtension = path.extname(fileName);
+    if (fileExtension !== '.png') {
+      continue;
+    }
+
+    const filePath = path.join(directoryPath, fileName);
+    const lastModifiedTime = await fsUtils.getLastModifiedTime(filePath);
+    if (lastModifiedTime < timestamp) {
+      await fse.remove(filePath);
+    }
+  }
 }
