4f6e20c063c1fbba12edc039424eba424d9604c0
Axel Havukangas <axel@havukangas.fi>
2025-11-26 10:15:59 +0200

    refactor: update sharp dependency version to remove warmup

--- src/sharp.ts
+++ src/sharp.ts
@@ -1,5 +1,4 @@
 import type sharpType from "sharp";
-import * as funcUtils from "./utils/func";
 
 /**
  * Load sharp conditionally.
@@ -8,27 +7,7 @@ import * as funcUtils from "./utils/func";
  * when you might not want to load the whole library
  * at startup.
  */
-export const load: () => Promise<typeof sharpType> = funcUtils.memo(
-	async () => {
-		const sharp = (await import("sharp")).default;
-
-		await warmup(sharp);
-		return sharp;
-	},
-);
-
-/**
- * Warms up the sharp library, handling any warmup errors.
- */
-async function warmup(sharp: typeof sharpType): Promise<void> {
-	// First run might cause a xmllib error, run safe warmup
-	// See https://github.com/lovell/sharp/issues/1593
-	try {
-		await sharp(
-			Buffer.from(
-				`<svg xmlns="http://www.w3.org/2000/svg"><rect width="1" height="1" /></svg>`,
-				"utf-8",
-			),
-		).metadata();
-	} catch {}
+export async function load(): Promise<typeof sharpType> {
+	const sharp = (await import("sharp")).default;
+	return sharp;
 }
